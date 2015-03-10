9fd77b436ce989832788020739aefb75c65c0516
robertdfrench <frenchrd@ornl.gov>
2015-03-10 12:05:49 -0400

    Relabel functions in terms of vectors

--- main.c
+++ main.c
@@ -8,12 +8,12 @@ typedef struct vector_t {
   float z;
 } vector_t;
 
-typedef struct particle_container_t {
+typedef struct vector_container_t {
   vector_t* particle_array;
   size_t num_elements;
-} particle_container_t;
+} vector_container_t;
 
-vector_t create_particle(float x, float y, float z) {
+vector_t create_vector(float x, float y, float z) {
   vector_t p;
   p.x = x;
   p.y = y;
@@ -21,34 +21,32 @@ vector_t create_particle(float x, float y, float z) {
   return p;
 }
 
-void set_initial_conditions(particle_container_t pc, size_t cube_length) {
+void set_initial_positions(vector_container_t pc, size_t cube_length) {
   int i,j,k;
   int current_particle = 0;
-  float h = 1.0 / cube_length;
+  float h = 1.0 / (cube_length - 1);
   for(i = 0; i < cube_length; i++) {
     for(j = 0; j < cube_length; j++) {
       for(k = 0; k < cube_length; k++) {
-        pc.particle_array[current_particle] = create_particle(h * i, h * j, h * k);
+        pc.particle_array[current_particle] = create_vector(h * i, h * j, h * k);
         current_particle++;
       }
     }
   }
 }
 
-#define CUBED(X) X * X * X
-// Allocates cube_length^3 particles along the unit cube
-particle_container_t allocate_particles(size_t cube_length) {
-  particle_container_t pc;
-  pc.particle_array = (vector_t *)malloc(sizeof(vector_t) * CUBED(cube_length));
-  pc.num_elements = CUBED(cube_length);
-  return pc;
+vector_container_t allocate_vectors(size_t num_elements) {
+  vector_container_t vc;
+  vc.particle_array = (vector_t *)malloc(sizeof(vector_t) * num_elements);
+  vc.num_elements = num_elements;
+  return vc;
 }
 
 void save_vector_to_file(vector_t p, FILE* f) {
   fprintf(f, "%f,%f,%f\n", p.x, p.y, p.z);
 }
 
-void save_particles_to_file(particle_container_t pc, FILE* f) {
+void save_vectors_to_file(vector_container_t pc, FILE* f) {
   fprintf(f, "x,y,z\n");
   int current_particle;
   for(current_particle = 0; current_particle < pc.num_elements; current_particle++) {
@@ -56,10 +54,12 @@ void save_particles_to_file(particle_container_t pc, FILE* f) {
   }
 }
 
+#define CUBE_LENGTH 10
+#define NUM_PARTICLES CUBE_LENGTH * CUBE_LENGTH * CUBE_LENGTH 
 int main(int argc, char** argv) {
-  particle_container_t pc = allocate_particles(10);
-  set_initial_conditions(pc, 10);
+  vector_container_t positions = allocate_vectors(NUM_PARTICLES);
+  set_initial_positions(positions, CUBE_LENGTH);
   FILE* f = fopen("sph_output.csv","w");
-  save_particles_to_file(pc, f);
+  save_vectors_to_file(positions, f);
   return 0;
 }
