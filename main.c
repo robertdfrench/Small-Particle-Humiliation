be22cf1cf2dd5399670a4888f048321a1797a3d2
robertdfrench <frenchrd@ornl.gov>
2015-03-10 11:18:47 -0400

    Configure and print initial conditions

--- main.c
+++ main.c
@@ -44,6 +44,22 @@ particle_container_t allocate_particles(size_t cube_length) {
   return pc;
 }
 
+void save_particle_to_file(particle_t p, FILE* f) {
+  fprintf(f, "%f,%f,%f\n", p.x, p.y, p.z);
+}
+
+void save_particles_to_file(particle_container_t pc, FILE* f) {
+  fprintf(f, "x,y,z\n");
+  int current_particle;
+  for(current_particle = 0; current_particle < pc.num_elements; current_particle++) {
+    save_particle_to_file(pc.particle_array[current_particle], f);
+  }
+}
+
 int main(int argc, char** argv) {
+  particle_container_t pc = allocate_particles(10);
+  set_initial_conditions(pc, 10);
+  FILE* f = fopen("sph_output.csv","w");
+  save_particles_to_file(pc, f);
   return 0;
 }
